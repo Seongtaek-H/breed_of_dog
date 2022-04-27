87e910d7cad58b1be7e384ea801497e19eb89c52
seongtaek <hst1993@hanmail.net>
2022-04-27 21:44:17 +0900

    SEO 수정

--- components/Seo.js
+++ components/Seo.js
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Seo({ title }) {
   return (
     <Head>
-      <title> {title} | breed of dog</title>
+      <title>{title}|나에게_어울리는_견종은?</title>
     </Head>
   );
 }
